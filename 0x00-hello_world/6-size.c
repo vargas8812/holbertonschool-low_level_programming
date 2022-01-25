3788c32901b294cebb790149ab8f722da1e59060
root <wilmerjvargas@gmail.com>
2022-01-25 13:16:05 -0800

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+*I'm so sick of this dumb handicap
+*/int main(void)
+{printf("Size of a char is  %zu byte\n", sizeof(char));
+printf("Size of a int is %zu byte\n", sizeof(int));
+printf("Size of a long is %zu byte\n", sizeof(long int));
+printf("Size of a long long is %zu byte\n", sizeof(long long int));
+printf("Size of a float is %zu byte\n", sizeof(float));
+return (0);
+}
