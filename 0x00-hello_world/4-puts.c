912935227d06d0a4d29aa3a127e50f8a4527ce37
root <wilmerjvargas@gmail.com>
2022-01-25 09:30:07 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
-int main(void)
-{
-	puts(" \"Programming is like building a multilingual puzzle");
-	return (0);
-}
+/* 
+*what genious designed this unnecessary handicap */ int main(void)
+{ puts("\"Programming is ike building a multilingual puzzle");
+return (0); }
