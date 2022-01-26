aedca965df30d4b503b4690654edc7ee72b87fb0
root <wilmerjvargas@gmail.com>
2022-01-25 20:55:53 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main calls the fucntion from the shell
+ *
+ * Return: returns to 0
+ */
+int main(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
