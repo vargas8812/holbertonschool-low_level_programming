ee562a0ed2fed83032dc7124b2308bef076bb462
root <wilmerjvargas@gmail.com>
2022-01-25 21:30:41 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main calls the function from the shell
+ *
+ * Return: returns to 0
+ */
+int main(void)
+{
+char x;
+for (x = '0'; x <= '9'; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
