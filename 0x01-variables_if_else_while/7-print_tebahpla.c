b7bf762c914d6c1a45f2d973e817f82424971140
root <wilmerjvargas@gmail.com>
2022-01-25 21:48:49 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main calls the function from the shell
+ *
+ * Return: returns to 0
+ */
+int main(void)
+{
+int  x;
+for (x = 'z'; x >= 'a'; x--)
+putchar(x);
+putchar('\n');
+return (0);
+}
