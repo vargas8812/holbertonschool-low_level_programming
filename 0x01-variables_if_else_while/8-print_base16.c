785b18b21d58d84f3b1e8b61a1d8a0d5fb7cf819
root <wilmerjvargas@gmail.com>
2022-01-25 21:57:57 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Desc of value
+ */
+int main(void)
+{
+int x;
+char y;
+for (x = 0; x < 10; x++)
+putchar(y);
+putchar('\n');
+return (0);
+}
