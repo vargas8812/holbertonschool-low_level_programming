f84b96dacf4466c352ad917dd091f71e299c081b
root <wilmerjvargas@gmail.com>
2022-01-30 12:32:20 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ for (m = 0; m < 60; m++)
 {
 _putchar((h / 10) + '0');
 _putchar((h % 10) + '0');
-_putchar(';');
+_putchar(':');
 _putchar((m / 10) + '0');
 _putchar((m % 10) + '0');
 _putchar('\n');
